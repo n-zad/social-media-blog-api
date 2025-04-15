a5f65cc6355686953a95fc85320926ddaf1c128d
Nickzad Bayati <nickzadbayati@gmail.com>
2025-04-15 05:22:58 +0000

    Implemented AccountDAO

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,68 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAO {
+    
+    /**
+     * Retrieve a specific account using its username and password.
+     *
+     * @param username the username of the account
+     * @param password the password of the account
+     * @return Account an Account object that includes the account_id
+     */
+    public Account getAccount(String username, String password) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE username = ? AND password = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                Account account = new Account(rs.getInt("account_id"), rs.getString("username"),
+                        rs.getString("password"));
+                return account;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Add an account record into the database which matches the values contained in the flight object.
+     *
+     * @param account an account object modelling an Account, not containing an account ID
+     * @return Account the account object that was successfully inserted into the database
+     */
+    public Account insertAccount(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if (pkeyResultSet.next()) {
+                int generated_account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
